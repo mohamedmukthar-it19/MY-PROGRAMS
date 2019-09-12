52526941f9c0be3d1c828bdb82ace8e5b3c82320
mohamedmukthar-it19 <mohamedmukthar.it19@bitsathy.ac.in>
2019-09-12 12:10:09 +0530

    fact

---
+++ factorial.c
@@ -0,0 +1,19 @@
+
+
+
+#include<stdio.h>
+void main()
+{
+int fact=1,i,n;
+printf("enter the number:");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+fact*=i;
+}
+printf("the factorial is %d",fact);
+}
+
+
+
+
